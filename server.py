c7217c025e196462bf436a0d3c27d450a14f113e
Chris.Zhou <jz200@duke.edu>
2019-04-14 23:55:25 -0400

    Added error messages, json validation. Closes #2

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
-
+error_messages = {1: 'Please put post request json in correct format',
+                2: 'Incorrect data types in request',
+                3: 'Username or request id does not exist'}
+processing_choices = ['histogram_eq', 'contrast_str', 'log_compress', 'reverse_vid']
+img_format_choices = ['JPEG', 'PNG', 'TIFF']
 @app.route("/api/process_img", methods=["POST"])
 def process_img_handler():
     r = request.get_json()
-    validate_process_img(r)
+    flag = validate_process_img(r)
     # decode image
     # process individual image
     # convert to desired format
@@ -31,4 +35,18 @@ def user_metrics_handler(username):
     pass
 
 def validate_process_img(r):
-    pass
+    try:
+        username = r['username']
+        num_img = r['num_img']
+        imgs = r['imgs']
+        processing = r['processing']
+        img_format = r['img_format']
+    except KeyError:
+        return 1
+    if ~((processing in processing_choices) and (img_format in img_format_choices)):
+        
+    try:
+        num_img = int(num_img)
+    except ValueError:
+        return 2
+    return flag
