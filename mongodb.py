1c8c4a9d0ac114ff67d559b14701d76d43dec1a3
Nicolas Hu <46500945+NicolasHu11@users.noreply.github.com>
2019-04-17 11:32:44 -0400

    update db structure

--- mongodb.py
+++ mongodb.py
@@ -9,49 +9,60 @@ connect("mongodb+srv://bme547:bme547_@cluster0-htwfk.mongodb.net/547")
 # database module
 # actions
 class Actions(EmbeddedMongoModel):
-    actions = fields.ListField(blank=True)
-
+    actions = fields.ListField()
+    processing_types = fields.ListField(blank=True)
+    timestamps = fields.TimestampField(blank=True)
+    processing_time = fields.IntegerField(blank=True)
+    image_size = fields.IntegerField(blank=True)
 
 # images
 class Images(EmbeddedMongoModel):
     """
 
     """
-    images_uploaded = fields.ListField(blank=True)  # each one is a char
-    images_processed = fields.ListField(blank=True)
-    images_format = fields.ListField(blank=True)
+    images_uploaded = fields.CharField()  
+    images_processed = fields.CharField()
+    images_format = fields.CharField(blank=True)
     images_procedures = fields.ListField(blank=True)
-    timestamps = fields.TimestampField(blank=True)
+    
 
 
 # User
 class User(MongoModel):  # this means a collection in db
 
     username = fields.CharField(blank=True)
-
+    # metrics
     num_actions = fields.IntegerField(blank=True)
     last_image_uploaded = fields.CharField(blank=True)
     request_id = fields.CharField(blank=True)
     # embedded documents
-    images = fields.EmbeddedDocumentField(Images)
-    actions = fields.EmbeddedDocumentField(Actions)
+    images = fields.EmbeddedDocumentListField(Images)
+    actions = fields.EmbeddedDocumentListField(Actions)
 
 
-def create_new_user(d):
+def query_user(username):
+    return User.objects.raw({"username": username}).first()
+
+
+def create_new_user(username):
 
     """
     create a new user
     username must be given
 
     Args:
-        d(dict): input dictionary, must contains 'username'
+        username(str): username
 
     Returns:
 
     """
-    User(
-        d['username'],
-    ).save()
+    try:
+        query_user(username)
+        print('User "{}" already exists.'.format(username))
+    except:
+        User(
+            username,
+        ).save()
 
     pass
 
@@ -66,12 +77,13 @@ def query_field(username, field):
         (type depends): one field in for given user
 
     """
-    user = User.objects.raw({"username": username}).first()
-    return getattr(user, field)
+    return getattr(query_user(username), field)
 
 
 def update_field(username, field, value):
-    user = Imageset.objects.raw({"_id": key}).first()
+    """not inclduing Images and Actions, value could be None"""
+    user = query_user(username)
+
     old_value = getattr(user, field)
     if isinstance(old_value, list):
         old_value.append(value)
@@ -82,17 +94,18 @@ def update_field(username, field, value):
     pass
 
 
-def delete_field(username, field):
-    user = User.objects.raw({"username": username}).first()
-    setattr(user, field, None)
-    user.save()
-    pass
-
-
 def query_user_metrics(username):
+    user = query_user(username)
     l = ['num_actions', 'last_image_uploaded', 'request_id']
-    user = User.objects.raw({"username": username}).first()
     metrics = []
     for field in l:
         metrics.append(getattr(user, field))
     return metrics
+
+
+def upload_images(username, raw_img, pro_img):
+    user = query_user(username)
+    user.last_image_uploaded = raw_img  # save as the last image
+    user.images.append(Images(raw_img,pro_img))
+    user.save()
+    pass
