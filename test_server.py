132832df0155e6a1c09c108a922286ec2aa2309a
Chris.Zhou <jz200@duke.edu>
2019-04-27 18:20:40 -0400

    Implemented half of test_server.py outline

--- test_server.py
+++ test_server.py
@@ -10,18 +10,49 @@ def load_img():
     b64_string = str(b64_bytes, encoding='utf-8')
     return b64_string
 
-def test_encode_imgs_b64():
-    pass
+@pytest.fixture
+def sample_img_request(load_img):
+    r = {'username': 'test088',
+         'num_img': 2,
+         'imgs': [load_img, load_img],
+         'procedure': 'histogram_eq',
+         'img_format': 'JPG',
+         'filename': 'airplane001.jpg'
+         }
+    return r
 
-def test_decode_imgs_from_request():
-    pass
+def test_encode_imgs_b64(load_img):
+    from server import decode_b64, encode_imgs_b64
+    decoded = decode_b64(load_img, 'JPG')
+    list_decoded = [decoded, decoded]
+    list_encoded = encode_imgs_b64(list_decoded)
+    assert list_encoded[0][0:10] == load_img[0:10]
 
-def test_process_igs_with_method():
-    pass
+def test_decode_imgs_from_request(sample_img_request):
+    from server import decode_imgs_from_request
+    decoded_list = decode_imgs_from_request(sample_img_request)
+    first_img = decoded_list[0]
+    # passes if the shape of the matrix is correct
+    assert first_img.shape[2] == 3
+
+
+def test_process_imgs_with_method(load_img):
+    from server import process_imgs_with_method, decode_b64
+    print(load_img)
+    decoded_list = [decode_b64(load_img, 'JPG')]
+    processed_list = process_imgs_with_method(decoded_list, 'histogram_eq')
+    decoded = decoded_list[0]
+    processed = processed_list[0]
+    # Only tests that the processed images are different from the original
+    assert not (decoded == processed).all()
+
 
 def test_generate_request_id():
     from server import generate_request_id
-    pass
+    id1 = generate_request_id()
+    id2 = generate_request_id()
+    difference = int(id2) - int(id1)
+    assert difference == 1
 
 def test_decode_b64(load_img):
     from server import decode_b64, encode_b64
@@ -44,10 +75,15 @@ def test_encode_b64(load_img):
     assert encoded[0:10] == load_img[0:10]
 
 def test_previous_request_preview():
-    pass
+    from server import previous_request_preview
+    data = previous_request_preview('test088', ['1'])
+    assert data['1']['filename'] == 'airplane001.jpg'
 
 def test_get_previous_requests():
-    pass
+    from server import get_previous_requests
+    previous_requests = get_previous_requests('test088')
+    # only checks if id 1 is among previous request ids
+    assert '1' in previous_requests
 
 def test_validate_previous_request():
     pass
